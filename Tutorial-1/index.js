e3eb37ba3c092f7c785478b5dca8755ed0b79845
V-M-B <vmbvarun@gmail.com>
2024-09-03 21:45:56 +0530

    Js Basic

---
+++ Tutorial-1/index.js
@@ -0,0 +1,11 @@
+//variable and scope 
+console.log("vmb")
+const accountid=144;
+let accountEmail="hitesh.com";
+var accountCity="bangalore";
+
+accountEmail="hitesh.com";
+let accountpass="benagaluru";
+
+console.log(accountid);
+console.table([accountid, accountEmail, accountpass, accountCity]);
---
+++ Tutorial-2/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const { stringify } = require("querystring");
+
+ //tret all JS code as newer version 
+
+// alert(3+2)/we are using nodejs not browser
+
+//data-type
+let name="vmv"
+let age=18
+let isloggedIn=false
+
+// Number
+// bitint
+// string
+// Boolean
+// null
+// undefined
+// symbol=unique
+
+// object 
+console.log(typeof age);
+
