bb9f15ba82a08a74dfa40ab018262ec6c7f5789d
V-M-B <vmbvarun@gmail.com>
2024-09-18 20:54:56 +0530

    Js Scope

---
+++ Tutorial-13/index.js
@@ -0,0 +1,23 @@
+//scope
+//global Scope
+//inner/block Scope
+
+
+
+{}//scope
+
+if (true) {
+
+    let a=10
+    const b=20
+    var c=30//scope-issue
+        
+}
+
+
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
