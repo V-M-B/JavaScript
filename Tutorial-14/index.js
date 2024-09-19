a327c86007f86313042c71046e94c49c8ff6b04d
V-M-B <vmbvarun@gmail.com>
2024-09-19 23:58:24 +0530

    Scope level and mini hoisting

---
+++ Tutorial-14/index.js
@@ -0,0 +1,47 @@
+function one() {
+  const username = "xyz";
+
+  function two() {
+    const website = "yt";
+    //console.log(username);
+  }
+  //console.log(website);//outside scope
+
+  two();
+}
+
+one();
+
+
+// if (true) {
+//     const user="abx"
+
+//     if (username=="abx") {
+//         const website="yt"
+//        // console.log(user+website);
+        
+//     }
+//     // console.log(website);
+    
+// }
+// // console.log(username);
+
+
+
+
+//*************************************************** */
+
+function add1(num) {
+    return num+1
+    
+}
+
+console.log(add1(4));
+ 
+
+
+
+
+const add2=function t(n) {
+    return num+2
+}
