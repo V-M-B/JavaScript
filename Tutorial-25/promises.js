bebe1303815d42a7934a1ef5a322932aec0a259d
V-M-B <vmbvarun@gmail.com>
2024-10-02 19:41:31 +0530

    Promises,async,await

--- Tutorial-25/promises.js
+++ Tutorial-25/promises.js
@@ -1,71 +1,119 @@
 //most of the time promises are used , and we also create it
 
- const promise1=new Promise(function (resolve, reject) 
- {
-    //DO ASYN TASK
-    //BD CALLS,CRYPTOGRAPHY,
-    setTimeout(function () {
-        console.log('async task is completed');
-        // resolve is a mtd
-        resolve()//cnted now with .then
-    },1000)
- })//promise created
+const { response } = require("express");
 
- promise1.then(function(){
-    console.log("promise consumed");
-    
- })
+const promise1 = new Promise(function (resolve, reject) {
+  //DO ASYN TASK
+  //BD CALLS,CRYPTOGRAPHY,
+  setTimeout(function () {
+    console.log("async task is completed");
+    // resolve is a mtd
+    resolve(); //cnted now with .then
+  }, 1000);
+}); //promise created
 
+promise1.then(function () {
+  console.log("promise consumed");
+});
 
- //method 2
- new Promise(function (resolve,reject) {
-    setTimeout(function () {
-        console.log("asy method 2");
-        resolve()
-    },1000)
- }).then(function () {
-    console.log("asyn 2 resolved");
-    
- })
+//method 2
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("asy method 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("asyn 2 resolved");
+});
 
+//Promies 3
 
- //Promies 3
+const promies3 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "coffee", email: "coffee@cf.com" });
+  }, 100);
+});
 
- const promies3=new Promise(function (resolve,reject){
-    setTimeout(function() {
-        resolve({username:"coffee",email:"coffee@cf.com"})
-    },100)
- })
+promies3.then(function (user) {
+  console.log(user);
+});
 
- promies3.then(function(user){
+//promies 4
+const promies4 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "superman", job: "fly" });
+    } else {
+      reject("ERROR !");
+    }
+  }, 1000);
+});
+
+promies4
+  .then((user) => {
     console.log(user);
-    
- })
-
-
- //promies 4
- const promies4=new Promise (function(resolve,reject){
-    setTimeout(function(){
-        let error=false
-        if(!error){
-            resolve({username:"spider-man",job:"web-genrator"})
-        }
-        else{
-            reject('ERROR !')
-        }
-    },1000)
- })
-
- promies4
- .then((user)=>{
-    console.log(user);
-    return user.username
-    
- }).then((username)=>{
+    return user.username;
+  })
+  .then((username) => {
     console.log(username);
-    
- }).catch(function (error) {
+  })
+  .catch(function (error) {
     console.log(error);
-    
-    
- })
+  })
+  .finally(() => {
+    console.log("The promies either resolved or rejected");
+  });
+
+//promise-5 asyn await => it is same as .then and .await
+
+const promisefive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "java", job: "suntv" });
+    } else {
+      reject("ERROR !");
+    }
+  }, 1000);
+});
+
+async function consumePromisefive() {
+  try {
+    const response = await promisefive;
+    console.log(response);
+  } catch (error) {
+    console.log("error in promies5");
+  }
+}
+consumePromisefive();
+
+//async mtd
+
+async function getAllusers(p) {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E:", Error);
+  }
+}
+// getAllusers()
+
+//same in try catch
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then((response)=>{
+   return response.json()
+})
+.then((data)=>{
+   console.log(data);
+   
+})
+
+.catch((error)=>{
+   console.log("error in");
+   
+})
