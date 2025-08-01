05de8c72cd86a6db69c55c2f96fefc1af9632b8b
V-M-B <vmbvarun@gmail.com>
2025-08-01 23:32:05 +0530

    Add JavaScript tutorials covering IIFE, control flow, loops, higher-order functions, DOM manipulation, events, promises, and API requests
    - Implement Immediately Invoked Function Expressions (IIFE) in Tutorial 16
    - Introduce control flow concepts including if-else statements and switch cases in Tutorial 17
    - Cover various loop types (for, while, do-while) in Tutorial 18
    - Explore higher-order functions like forEach and map in Tutorial 19
    - Create basic HTML structure for DOM manipulation in Tutorial 21
    - Demonstrate event handling and propagation in Tutorial 22
    - Implement setTimeout and setInterval for asynchronous operations in Tutorial 23
    - Make API requests using XMLHttpRequest in Tutorial 24
    - Introduce promises and async/await for handling asynchronous operations in Tutorial 25
    - Enhance array manipulation techniques in Tutorials 3 to 9

---
+++ Tutorial-2/Chapter01/index.js
@@ -0,0 +1,19 @@
+// string
+console.log('Hello, World!');
+
+let greeting = 'Hello, World!';
+console.log(greeting);
+
+// string concat
+let firstname='John';
+let lastname = 'Doe';
+
+let fullname =  firstname + ''+ lastname;
+console.log(fullname);
+
+// getting length of string
+let str = 'Hello, World!';
+console.log(str.length);
+
+// accessing characters in a string
+console.log(str[0]); // H
